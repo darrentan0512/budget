8043670aa0ca3b4737b8065f1f3ceb6ddbddc631
darrentan <darren.tan0512@gmail.com>
2023-08-24 22:36:16 +0800

    feat: refactor entryline component

--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { Checkbox, Form, Segment } from 'semantic-ui-react';
+import { Form } from 'semantic-ui-react';
 import ButtonSaveOrCancel from './ButtonSaveOrCancel';
+import EntryForm from './EntryForm';
 
 function NewEntryForm({ addEntry }) {
 
@@ -12,35 +13,14 @@ function NewEntryForm({ addEntry }) {
 
     return (
         <Form unstackable>
-            <Form.Group>
-                <Form.Input
-                    icon='tags'
-                    width={12}
-                    label='Description'
-                    placeholder='New Shinny Thing'
-                    value={description}
-                    onChange={(event) => setDescription(event.target.value)}
-                />
-
-                <Form.Input
-                    width={4}
-                    label='Value'
-                    placeholder="100.00"
-                    icon="dollar"
-                    iconPosition='left'
-                    value={value}
-                    onChange={(event) => setValue(event.target.value)}
-                ></Form.Input>
-            </Form.Group>
-
-            <Segment compact>
-                <Checkbox
-                    toggle 
-                    label='Is Expense?'
-                    checked={isExpense}
-                    onChange={() => setIsExpense(oldState => !oldState)}
-                    />
-            </Segment>
+            <EntryForm 
+                description={description} 
+                value={value} 
+                isExpense={isExpense}
+                setDescription = {setDescription} 
+                setValue = {setValue}
+                setIsExpense = {setIsExpense}
+            />
             <ButtonSaveOrCancel addEntry={addEntry} description={description} value={value} isExpense={isExpense}/>
         </Form>
     )

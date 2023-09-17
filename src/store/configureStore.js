fb80b54812704b3bd6898fd48647a3f98bf15db1
darrentan <darren.tan0512@gmail.com>
2023-09-18 02:12:24 +0800

    understanding saga effect

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,14 +4,14 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import entriesReducer from '../reducers/entries.reducer';
 import modalsReducer from '../reducers/modals.reducer';
 import createSagaMiddleware from 'redux-saga';
-
+import { testSaga } from '../sagas/testSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 const middleWares = [sagaMiddleware];
 
 
 const configureStore = () => {
-    return createStore(
+    const store =  createStore(
         combineReducers({
             entries: entriesReducer,
             modals: modalsReducer,
@@ -19,7 +19,9 @@ const configureStore = () => {
         composeWithDevTools(
             applyMiddleware(...middleWares)
         )
-    )
+    );
+    sagaMiddleware.run(testSaga);
+    return store;
 };
 
 export default configureStore;

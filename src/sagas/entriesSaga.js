b0037372bcde6fa3d73b257cffda50892109e542
darrentan <darren.tan0512@gmail.com>
2023-10-09 00:51:51 +0800

    edit entries saga

--- src/sagas/entriesSaga.js
+++ src/sagas/entriesSaga.js
@@ -1,4 +1,4 @@
-import { take, call, put} from "redux-saga/effects";
+import { take, call, put, fork} from "redux-saga/effects";
 import types from '../actions/entries.actions'
 import axios from "axios";
 
@@ -8,4 +8,17 @@ export function* getAllEntries() {
     const result = yield call(axios, 'http://localhost:3002/entries')
     console.log(result);
     yield put({type: types.POPULATE_ENTRIES, payload: result.data})
+}
+
+function* getEntryDetails(id){
+    const { data } = yield call(axios, `http://localhost:3002/values/${id}`); 
+    console.log(data);
+}
+
+export function* getAllEntriesDetails() {
+    const { payload } = yield take(types.POPULATE_ENTRIES);
+    for (let index = 0; index < payload.length; index++) {
+        const entry = payload[index];
+        yield fork(getEntryDetails, entry.id);
+    }
 }
